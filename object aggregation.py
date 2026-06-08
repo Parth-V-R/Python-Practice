0e1b9f9b97aab7edb52df8fd3adb4e38951ba8a4
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-06-08 23:20:17 +0530

    method print_address added to class Customer

--- object aggregation.py
+++ object aggregation.py
@@ -5,11 +5,19 @@ class Customer:
         self.gender=gender
         self.address=address
         
+    def print_address(self):
+        print(self.address.get_city(),self.address.pincode,self.address.state)
+        
 class Address:
     def __init__(self,city,state,pincode):
-        self.city=city
+        self.__city=city
         self.state=state
         self.pincode=pincode
         
+    def get_city(self):
+        return self.__city
+        
 add1=Address('Amravati','Maharashtra',444606)
 cus=Customer('Parth','Male',add1)
+
+cus.print_address() 

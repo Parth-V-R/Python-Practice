b2711803ba97fbb7cfbd396f69094ee04a1cddd5
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-06-09 01:13:38 +0530

    child class "Student" created and called methods from Parent Class

--- inheritance.py
+++ inheritance.py
@@ -1,7 +1,19 @@
 class User:
     def __init__(self):
         self.name='Parth'
+        self.gender='Male'
         
     def login(self):
         print("Login")
     
+class Student(User):
+        
+    def enroll(self):
+        print("Enroll into the course")
+        
+u=User()
+s=Student()
+
+print(s.name)
+s.login()
+s.enroll()

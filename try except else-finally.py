682753d4b6dc9e0ffea315b7c33423c7c629f7f6
Parth Rajurkar <pvrajurkar1@gmail.com>
2026-06-13 23:24:37 +0530

    finally block added

--- try except else-finally.py
+++ try except else-finally.py
@@ -1,5 +1,5 @@
 try:
-    f=open('Samplefile1.txt','r')
+    f=open('Samplefile10.txt','r')
 
 except FileNotFoundError:
     print("File Not Found")
@@ -10,3 +10,7 @@ except Exception:
 #When error not exists,else part is executed,if error found then except part is executed    
 else:
     print(f.read())
+
+# Finally will  always get executed,whether error is there or not    
+finally:
+    print("This will always get executed")
